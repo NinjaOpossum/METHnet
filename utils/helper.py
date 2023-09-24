b0cc36373e082d118ebc2bdc3018b4401ef963db
Max.Przy <104021614+NinjaOpossum@users.noreply.github.com>
2023-09-24 12:01:46 +0200

    Update helper.py
     added some plot funktions to visualize data

--- utils/helper.py
+++ utils/helper.py
@@ -15,14 +15,15 @@ def create_folder(folder):
             os.mkdir(current_path)
 
 # @MPR
+# added some plot funktions to visualize data
 def plot_loss(log_dir, title):
     df = pd.read_csv(os.path.join(log_dir, "results.csv"))
     epochs = df["Epoch"]
     train_losses = df["Train Loss"]
-    validation_losses = df["Validation Loss"]
-    dice_scores = df["Validation Dice Score"]
+    validation_losses = df["Val Loss"]
+    dice_score = df["Val Dice Score"]
     
-    fig, axes = plt.subplots(1, 2, figsize = (20, 7))
+    fig, axes = plt.subplots(1, 2, figsize = (25, 7))
     axes[0].set_xlabel("Epoch")
     axes[0].set_ylabel("Loss")
     axes[0].grid(True)
@@ -35,21 +36,71 @@ def plot_loss(log_dir, title):
     axes[1].set_ylabel("Dice Score")
     axes[1].grid(True)
     axes[1].set_xticks(epochs)
-    axes[1].plot(dice_scores, label = "Validation Dice Score", color = "red")
+    axes[1].plot(dice_score, label = "Overall Val Dice Score", color = "red")
+    axes[1].set_ylim((0, 1))
+    axes[1].legend()
+    fig.suptitle(title)
+    fig.set_facecolor("white")
+    fig.savefig(os.path.join(log_dir, "results_loss.png"))
+
+def plot_dices(log_dir, title):
+    df = pd.read_csv(os.path.join(log_dir, "results.csv"))
+    epochs = df["Epoch"]
+    dice_by_class_start_index =  df.columns.get_loc('Val Recall') + 1
+    dice_scores = df["Val Dice Score"]
+
+    fig, ax = plt.subplots(1, figsize = (20, 7))
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Dice Score")
+    ax.grid(True)
+    ax.set_xticks(epochs)
+    ax.plot(dice_scores, label = "Overall Val Dice Score", ls = "--", linewidth=3)
+    for cls in df.columns[dice_by_class_start_index:]:
+        ax.plot(df[cls], label = cls.split("_")[1] + "_DS")
+    ax.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
+    fig.suptitle(title)
+    fig.set_facecolor("white")
+    fig.subplots_adjust(right=0.8)
+    fig.savefig(os.path.join(log_dir, "results_ind_dice.png"))
+
+def plot_accuracy(log_dir, title):
+    df = pd.read_csv(os.path.join(log_dir, "results.csv"))
+    epochs = df["Epoch"]
+    train_accuracies = df["Train Accuracy"]
+    train_foreground_accuracies = df["Train Foreground Accuracy"]
+    val_accuracies = df["Val Accuracy"]
+    val_foreground_accuracies = df["Val Foreground Accuracy"]
+    
+    fig, axes = plt.subplots(1, 2, figsize = (20, 7))
+    axes[0].set_xlabel("Epoch")
+    axes[0].set_ylabel("Accuracy")
+    axes[0].grid(True)
+    axes[0].set_xticks(epochs)
+    axes[0].plot(train_accuracies, label = "Train Accuracy")
+    axes[0].plot(val_accuracies, label = "Validation Accuracy")
+    axes[0].legend()
+    axes[0].set_ylim((0, 1))
+    
+    axes[1].set_xlabel("Epoch")
+    axes[1].set_ylabel("Foreground Accuracy")
+    axes[1].grid(True)
+    axes[1].set_xticks(epochs)
+    axes[1].plot(train_foreground_accuracies, label = "Train Accuracy")
+    axes[1].plot(val_foreground_accuracies, label = "Validation Accuracy")
     axes[1].legend()
     axes[1].set_ylim((0, 1))
     fig.suptitle(title)
     fig.set_facecolor("white")
-    fig.savefig(os.path.join(log_dir, "results.png"))
+    fig.savefig(os.path.join(log_dir, "results_accuracy.png"))
 
-def plot_examples(logdir, images, annotations, preds, identifier):
+def plot_examples(logdir, images, annotations, preds, identifier, n_classes):
     inference_folder = os.path.join(logdir, "inferences")
     create_folder(inference_folder)
     for i in range(images.shape[0]):
         plt.imsave(os.path.join(inference_folder, f"{identifier}{i}_image.png"), 
                    np.transpose(images[i], [1, 2, 0]))
-        # TODO: Must update the following lines as if annotations have multiple channels for the future
+        # TODO: Update the following lines as if annotations have multiple channels for the future
         plt.imsave(os.path.join(inference_folder, f"{identifier}{i}_annotation.png"), 
-                   np.squeeze(annotations[i]), vmin = 0, vmax = 1)
+                   annotations[i], vmin = 0, vmax = n_classes)
         plt.imsave(os.path.join(inference_folder, f"{identifier}{i}_pred.png"), 
-                   np.squeeze(preds[i]), vmin = 0, vmax = 1)
+                   preds[i], vmin = 0, vmax = n_classes)
